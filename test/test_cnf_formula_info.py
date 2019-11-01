3c9fdb2849427e779f3d59dd37195bc063994fc0
Mateusz-gmail <mgrzel@student.agh.edu.pl>
2019-11-01 17:30:35 +0100

    Add test case

--- test/test_cnf_formula_info.py
+++ test/test_cnf_formula_info.py
@@ -158,3 +158,49 @@ class TestFOLInfo:
         # clauses are equivalent in mathematical sense
         assert info.number_of[fol.CNFClause] == 2
         assert info.number_of[fol.CNFFormula] == 1
+
+    def test_two_predicate_one_functor_two_atom_two_clauses(self):
+        p1 = fol.Predicate('p1', items=[])
+        f1 = fol.Functor('f1', items=[])
+        p2 = fol.Predicate('p1', items=[f1])
+        a1 = fol.Atom(items=[p1], connective=None)
+        a2 = fol.Atom(items=[p2], connective=None)
+        l1 = fol.Literal(items=a1, negated=False)
+        l2 = fol.Literal(items=a2, negated=False)
+        c1 = fol.CNFClause(items=[l1, l2])
+        c2 = fol.CNFClause(items=[l2])
+        f = fol.CNFFormula(items=[c1, c2])
+
+        info = f.get_info()
+
+        assert info.number_of_singleton_variables == 0
+        assert info.number_of_equality_atom_instances == 0
+        assert info.number_of_negated_literal_instances == 0
+        assert info.clause_lengths[1] == 1
+        assert info.clause_lengths[2] == 1
+        assert len(info.clause_lengths) == 2
+        assert info.number_of_horn_clauses_instances == 1
+        assert info.predicate_arities[0] == 1
+        assert info.predicate_arities[1] == 2
+        assert len(info.predicate_arities) == 2
+        assert info.functor_arities[0] == 2
+        assert len(info.functor_arities) == 1
+        assert info.term_instances_depths[0] == 2
+        assert len(info.term_instances_depths) == 1
+
+        assert info.number_of_instances[fol.Variable] == 0
+        assert info.number_of_instances[fol.Predicate] == 3
+        assert info.number_of_instances[fol.Functor] == 2
+        assert info.number_of_instances[fol.Atom] == 3
+        assert info.number_of_instances[fol.Literal] == 3
+        assert info.number_of_instances[fol.CNFClause] == 2
+        assert info.number_of_instances[fol.CNFFormula] == 1
+
+        assert info.number_of[fol.Variable] == 0
+        assert info.number_of[fol.Predicate] == 2
+        assert info.number_of[fol.Functor] == 1
+        assert info.number_of[fol.Atom] == 2
+        assert info.number_of[fol.Literal] == 2
+        # clauses are equivalent in mathematical sense
+        assert info.number_of[fol.CNFClause] == 2
+        assert info.number_of[fol.CNFFormula] == 1
